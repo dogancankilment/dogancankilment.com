b31b35696610d55b03d8daae9d50136cd02c7d89
dogancankilment <dogancankilment@gmail.com>
2019-12-06 14:41:48 +0300

    deleted unnecessary lines

--- base/views.py
+++ base/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.core.context_processors import csrf
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
-from django.http import HttpResponse
-from django.shortcuts import render
 
 
 def home(request):
     return render_to_response("home/index.html")
-
-
-def fitnesspointdemo(request):
-    return render_to_response("workspace/fitnesspoint/demo.html")
-
-
-def fitnesspointindex(request):
-    return render_to_response("workspace/fitnesspoint/index.html")
